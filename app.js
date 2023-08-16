a91cbef300769d0c22e0d1c2f985068d13d43be5
hemanth <kamadahemanth7l@gamail.com>
2023-08-16 12:52:53 +0000

    todolist

--- app.js
+++ app.js
@@ -25,24 +25,7 @@ const initializeDbAndServer = async () => {
     process.exit(1);
   }
 };
-app.use(express.json());
 
-let database = null;
-
-const initializeDbAndServer = async () => {
-  try {
-    database = await open({
-      filename: databasePath,
-      driver: sqlite3.Database,
-    });
-    app.listen(3000, () =>
-      console.log("Server Running at http://localhost:3000/")
-    );
-  } catch (error) {
-    console.log(`DB Error: ${error.message}`);
-    process.exit(1);
-  }
-};
 initializeDbAndServer();
 
 const hasPriorityAndStatusProperties = (requestQuery) => {
